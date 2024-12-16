7c7600c0214d79f554cd2cf395126a0c7cab0495
CodeX93 <xappifai@gmail.com>
2024-12-16 16:43:00 +0500

    configure backend deployment

--- backend/index.js
+++ backend/index.js
@@ -22,16 +22,6 @@ SparePartRouter
 dotenv.config();
 
 
-import path from 'path';
-
-// Serve React build files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 
 const app = express();
